c73b5109c9a8e68bda572ed240677476e498914f
FallenStarrr <juzopigeon@gmail.com>
2024-05-26 16:16:40 +0600

    start

---
+++ src/main/java/com/dragon/dragon_booking/DragonBookingApplication.java
@@ -0,0 +1,13 @@
+package com.dragon.dragon_booking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DragonBookingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DragonBookingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dragon/dragon_booking/DragonBookingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dragon.dragon_booking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DragonBookingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

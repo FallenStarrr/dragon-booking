6c453d2bce12d025fadf2425fea07b09a7ee671a
FallenStarrr <juzopigeon@gmail.com>
2024-05-29 07:20:33 +0600

    add new room interface method

--- src/main/java/com/dragon/dragon_booking/controller/RoomController.java
+++ src/main/java/com/dragon/dragon_booking/controller/RoomController.java
@@ -1,5 +1,29 @@
 package com.dragon.dragon_booking.controller;
 
+import java.math.BigDecimal;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
+
+import com.dragon.dragon_booking.model.Room;
+import com.dragon.dragon_booking.response.RoomResponse;
+import com.dragon.dragon_booking.service.IRoomService;
+
+import lombok.RequiredArgsConstructor;
+
+
+@RequiredArgsConstructor
 public class RoomController {
-  
+    
+   private final IRoomService roomService;
+   
+   public ResponseEntity<RoomResponse> addNewRoom(
+   @RequestParam("photo") MultipartFile  photo, 
+   @RequestParam("roomType") String roomType, 
+   @RequestParam("roomPrice") BigDecimal roomPrice) {
+            Room savedRoom = roomService.addNewRoom(photo, roomType, roomPrice);
+            RoomResponse response = new RoomResponse(savedRoom.getId(), savedRoom.getRoomType(), savedRoom.getRoomPrice());
+            return ResponseEntity.ok(response);
+    }
 }
--- src/main/java/com/dragon/dragon_booking/service/IRoomService.java
+++ src/main/java/com/dragon/dragon_booking/service/IRoomService.java
@@ -1,5 +1,11 @@
 package com.dragon.dragon_booking.service;
 
+import java.math.BigDecimal;
+
+import org.springframework.web.multipart.MultipartFile;
+
+import com.dragon.dragon_booking.model.Room;
+
 public interface IRoomService {
-  
+    Room addNewRoom(MultipartFile photo, String roomType, BigDecimal roomPrice);
 }
